0f4d646f4dac9a62336b8ef08778580c6e92c963
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-09-26 09:31:32 +0100

    Add mst algorithm

--- main.lua
+++ main.lua
@@ -1,5 +1,13 @@
-function love.load()
+require('src/utils/utils')
+MST = require('src/utils/mst')
 
+function love.load()
+    points = {{1,2},{2,3},{3,4}}
+    edges = {{1,2,2,3},{2,3,3,4},{3,4,1,2}}
+    tree = MST.tree(points,edges)
+    for i = 1, #tree do
+      print(tree[i][1],tree[i][2],tree[i][3],tree[i][4])
+    end
 end
 
 function love.update(dt)
